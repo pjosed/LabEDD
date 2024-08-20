f9c213f8a852b851e83ede03ebec575ee3dcc047
Jose Peña <jose102005p@gmail.com>
2024-08-19 22:38:37 -0500

    Se agrega el proyecto

---
+++ src/labedd/LabEDD.java
@@ -0,0 +1,10 @@
+package labedd;
+
+public class LabEDD {
+
+    public static void main(String[] args) {
+       InterfazGrafica InterfazGrafica = new InterfazGrafica();
+       InterfazGrafica.setVisible(true);
+    }
+    
+}
