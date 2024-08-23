a45096af6532799dc938d567083bcee992fdd3da
AnaMeza21 <mezaana@uninorte.edu.co>
2024-08-22 20:07:59 -0500

    Eliminar_producto1

---
+++ src/labedd/Eliminar_producto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labedd;
+
+/**
+ *
+ * @author Janus
+ */
+public class Eliminar_producto {
+    
+}
